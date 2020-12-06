f4b98d7affca913ef66f48b8e5a755fbecdf7a8f
Curt <curt.poff@gmail.com>
2020-12-05 23:04:33 -0700

    new post

--- src/containers/About.js
+++ src/containers/About.js
@@ -4,6 +4,6 @@ import React from 'react'
 export default () => (
   <div>
     <h1>This is what we're all about.</h1>
-    <p>React, static sites, performance, speed. It's the stuff that makes us tick.</p>
+    <p>I'm frustrated - fed up, actually - that Kevin Sumlin is still head coach. He hasn't improved the program at UofA at all, he's been paid a ton of public money, and he needs to go. I hope you'll join my campaign.</p>
   </div>
 )
