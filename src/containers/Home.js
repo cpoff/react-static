65511d9bb0bce1a98082980178c886a325ecbcf8
Curt <curt.poff@gmail.com>
2020-12-05 22:56:02 -0700

    new site

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -4,7 +4,9 @@ import logoImg from '../logo.png'
 
 export default () => (
   <div>
-    <h1 style={{ textAlign: 'center' }}>Welcome to</h1>
-    <img src={logoImg} alt="" />
+    <h1 style={{ textAlign: 'center' }}>Fire Kevin Sumlin!</h1>
+    <p>Kevin Sumlin's record in Tucson: 9-19, including a humiliating loss to Washington. 24-0 at halftime. 44-27 final but was never that close. Same goes for the UCLA game. Grant Gunnell injured on the first play and Cats were never in it. I've also seen commentary calling for AD Heeke to join Sumlin in the unemployment line. Can't say that I disagree there.</p>
+
+<p>Sumlin's contract pays $14.5 million over five years. Do the math. He's just not worth the money. Kevin Sumlin, his whole staff, and maybe even his boss need to go.</p>
   </div>
 )
